b77d252dc94c8df74641c215239d4372bcba2212
questras <michal.wisniewski422@gmail.com>
2022-08-29 14:59:22 +0200

    Add query execution to script.

--- deploy_kafka.sh
+++ deploy_kafka.sh
@@ -7,4 +7,5 @@ ssh st133@st133${vm}.rtb-lab.pl \
   "git clone https://github.com/pds-allezon/${name}.git;" \
   "cd ${name};" \
   "git pull;" \
-  "sudo docker-compose up -d;"
+  "sudo docker-compose up -d;" \
+  "sleep 20s ; sudo docker exec ksqldb-cli ksql --file /query/aggregates-table.sql -- http://ksqldb-server:8088;"
