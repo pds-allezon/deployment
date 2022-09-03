8f73dadbe76d219a2a74e60be469f901c95eb39f
questras <michal.wisniewski422@gmail.com>
2022-09-03 15:43:17 +0200

    fix missing vm.

--- deploy_kafka.sh
+++ deploy_kafka.sh
@@ -1,3 +1,5 @@
+vm="vm109"
+
 ssh st133@st133${vm}.rtb-lab.pl \
   "sudo mkdir -p /vol1/zk-data; sudo mkdir -p /vol2/zk-txn-logs; sudo mkdir -p /vol3/kafka-data;" \
   "sudo chown -R 1000:1000 /vol1/zk-data; sudo chown -R 1000:1000 /vol2/zk-txn-logs; sudo chown -R 1000:1000 /vol3/kafka-data;"
